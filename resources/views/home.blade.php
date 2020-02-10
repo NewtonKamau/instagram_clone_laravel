e67993b3e5448db3bab075ae86d84f3b40e71b84
newnyc <kamaunewton78@gmail.com>
2020-02-10 11:28:46 +0300

    Home UI
    created the insta clone UI with static images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,35 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+   <div class="row">
+       <div class="col-3 p-4">
+           <img src="download.png" class="rounded-circle">
+       </div>
+       <div class="col-9 pt-5">
+           <div><h1>Newton Developers</h1></div>
+           <div class="d-flex">
+           <div class="p-3"><strong>123</strong> posts</div>
+           <div class="p-3"><strong>24</strong> followers</div>
+           <div class="p-3"><strong>11</strong>following</div>
+           </div>
+           <div class="p-2 font-weight-bold">NewtonDeveloper.com</div>
+           <div>The creative developer with laravel and vue</div>
+           <div><a href="#">www.newtondevelopers.com</a> </div>
+       </div>
+   </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+    <div class="row pt-5">
+     <div class="col-4">
+         <img src="download.png" class="w-100">
+     </div>
+        <div class="col-4">
+            <img src="download.png" class="w-100">
         </div>
+        <div class="col-4">
+            <img src="download.png" class="w-100">
+        </div>
+
     </div>
+
 </div>
 @endsection
