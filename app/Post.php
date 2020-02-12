63a470d46494499e10848c77043a42c1dce390a8
newnyc <kamaunewton78@gmail.com>
2020-02-12 11:22:33 +0300

    Posts
    Created posts and did a relationship with user

---
+++ app/Post.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    //
+    public function users(){
+        return $this->belongsTo(User::class);
+    }
+}
