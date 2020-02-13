8f33567305d6c91fb8e12d40c9f778ef26f76d9e
newnyc <kamaunewton78@gmail.com>
2020-02-13 10:01:17 +0300

    added padding
    padding for the images

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -24,7 +24,7 @@
 
     <div class="row pt-5">
         @foreach($user->posts as $post)
-            <div class="col-4">
+            <div class="col-4 pb-4">
             <img src="/storage/{{$post->image}}" class="w-100">
             </div>
         @endforeach
