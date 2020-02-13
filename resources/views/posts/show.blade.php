0a09c2ac4c998ebff8f3d9a3234defb42852f268
newnyc <kamaunewton78@gmail.com>
2020-02-13 15:27:24 +0300

    showing post details to the user
    abd capturing username

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,7 +5,12 @@
         <div class="col-8">
             <image src="/storage/{{$post->image}}" class="w-100">
         </div>
+        <div class="col-4">
+            <div>
+            <h3>{{ $post->user->username }}</h3>
+            <p>{{$post->caption}}</p>
+            </div>
+        </div>
     </div>
-
 </div>
 @endsection
