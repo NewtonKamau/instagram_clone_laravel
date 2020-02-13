3cf1673993e5410c6445080816c4612dd40de3c3
newnyc <kamaunewton78@gmail.com>
2020-02-13 09:24:05 +0300

    Order post by time

--- app/User.php
+++ app/User.php
@@ -41,6 +41,6 @@ class User extends Authenticatable
     }
     public function posts()
     {
-        return $this->hasMany(Post::class);
+        return $this->hasMany(Post::class)->orderBy('created_at', 'DESC');
     }
 }
